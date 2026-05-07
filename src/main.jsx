20e8165dc37e9d527a55d19bd61d037c0d2d6bd3
chavanpramod-ui <pramodc431510@gmail.com>
2026-05-07 18:07:00 +0530

    all code

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,8 @@ import './index.css'
 import App from './App.jsx'
 import axios from 'axios'
 
-// --- CRITICAL: SET YOUR RENDER BACKEND URL HERE ---
-// Replace this placeholder with your actual Render URL
-// Make sure there is NO slash (/) at the very end of the URL!
-axios.defaults.baseURL = 'https://YOUR-BACKEND-NAME.onrender.com';
+
+axios.defaults.baseURL = 'https://profilehub-backend-5qqp.onrender.com';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
